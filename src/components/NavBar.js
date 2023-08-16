8be43d63ab177be4406114621da3399d84e5a177
hp1heloisa <heloisapimentel2202@gmail.com>
2023-08-16 18:20:51 -0300

    navbar com logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
 import { styled } from "styled-components"
 
 export default function NavBar() {
+
+    let [click, setClick] = useState(false);
+
+    const navigate = useNavigate();
+
+    function clickChange() {
+        if (click) {
+            setClick(false);
+        } else{
+            setClick(true);
+        }
+    }
+
+    function Profile() {
+        if (click) {
+            return(
+                <div>
+                    <div className="user" onClick={clickChange}>
+                        <ion-icon name="chevron-up-outline"></ion-icon>
+                        <img src="https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" />
+                    </div>
+                    <div className="logout">
+                        <span>Logout</span>
+                    </div>
+                </div>
+            )
+        } else {
+            return(
+                <div className="user" onClick={clickChange}>
+                    <ion-icon name="chevron-down-outline"></ion-icon>
+                    <img src="https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" />
+                </div>
+            )
+        }
+    }
+
     return(
         <ContainerGeral>
-            <h1>linkr</h1>
+            <h1 onClick={() => navigate('/home')}>linkr</h1>
             <div className="search">
                 <input placeholder="Search for people"/>
                 <ion-icon name="search"></ion-icon>
             </div>
-            <div className="user">
-                <ion-icon name="chevron-down-outline"></ion-icon>
-                <img src="https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" />
-            </div>
+            <Profile />
         </ContainerGeral>
     )
 }
@@ -23,6 +58,7 @@ const ContainerGeral = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 15px;
+    position: relative;
     h1{
         font-family: 'Passion One', cursive;
         color: white;
@@ -63,6 +99,25 @@ const ContainerGeral = styled.div`
             width: 50px;
             height: 50px;
             border-radius: 100%;
+        }
+    }
+    .logout{
+        font-family: 'lato';
+        font-size: 17px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        height: 47px;
+        width: 150px;
+        position: absolute;
+        right: 0;
+        bottom: -47px;
+        background-color: #151515;
+        border-bottom-left-radius: 20px;
+        span:hover{
+            text-decoration: underline;
+            cursor: pointer;
         }
     }
 `

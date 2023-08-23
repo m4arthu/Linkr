13742efec6f351d0db3c55eb37ce8a108373dced
Caio Pires <caiogodoi_92@hotmail.com>
2023-08-23 00:22:51 -0300

    follow finalizado

--- src/components/FollowButton.jsx
+++ src/components/FollowButton.jsx
@@ -1,33 +1,54 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-export default function FollowButton({ id, username, data }) {
+export default function FollowButton({ id }) {
   const [following, setFollowing] = useState(false)
-  console.log('idddddddddddddddd', id, username)
-  console.log('meus dados', data.id)
+  const [isRequesting, setIsRequesting] = useState(false)
 
   useEffect(() => {
+    setIsRequesting(true)
     axios
       .get(`${process.env.REACT_APP_API_URL}/checkFollow?followedId=${id}`)
-      .then(response => {
-        console.log('foll requisicao', response.data)
-        setFollowing(response.data)
-      })
+      .then(response => setFollowing(response.data.following))
       .catch(err => console.log(err))
-  }, [])
+      .finally(() => {
+        setIsRequesting(false)
+      })
+  }, [id])
 
   function handleFollow() {
+    if (isRequesting) {
+      return
+    }
+
+    setIsRequesting(true)
     axios
       .post(`${process.env.REACT_APP_API_URL}/follow`, { followedUserId: id })
       .then(response => {
-        console.log('dado da requisicao de seguir:', response.data)
+        if (response.data.message === 'Você começou a seguir o usuário.') {
+          return setFollowing(true)
+        } else {
+          return setFollowing(false)
+        }
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        alert(
+          'Não foi possível executar a operação. Tente novamente mais tarde.'
+        )
+      })
+      .finally(() => {
+        setIsRequesting(false)
+      })
   }
 
   return (
     <div>
-      <button onClick={handleFollow}>follow</button>
+      <button onClick={handleFollow} disabled={isRequesting}>
+        {isRequesting ? 'Aguarde...' : following ? 'Unfollow' : 'Follow'}
+      </button>
     </div>
   )
 }
+
+//

802060a045163ee8947e100ee8ca1ab5a0f7762a
Caio Pires <caiogodoi_92@hotmail.com>
2023-08-22 23:26:35 -0300

    função de seguir e deixar de seguir usuario

--- src/components/FollowButton.jsx
+++ src/components/FollowButton.jsx
@@ -1,13 +1,33 @@
-export default function FollowButton({id, username, data}) {
-  console.log('idddddddddddddddd', id, username, )
-  console.log('dataaaaaaaaaaa', data.id);
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
+export default function FollowButton({ id, username, data }) {
+  const [following, setFollowing] = useState(false)
+  console.log('idddddddddddddddd', id, username)
+  console.log('meus dados', data.id)
 
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/checkFollow?followedId=${id}`)
+      .then(response => {
+        console.log('foll requisicao', response.data)
+        setFollowing(response.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  function handleFollow() {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/follow`, { followedUserId: id })
+      .then(response => {
+        console.log('dado da requisicao de seguir:', response.data)
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <div>
-      
-      <button>Follow</button>
+      <button onClick={handleFollow}>follow</button>
     </div>
   )
 }
